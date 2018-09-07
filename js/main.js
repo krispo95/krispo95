c4f63b8eb190dec10498a8e6fe238143182ec69f
Phlogistonny <pogosyan.chris@gmail.com>
2018-09-08 00:51:25 +0300

    new animation

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//  jQuery(document).ready(function ($) {
+//       $("#fullpage").fullpage({
+//         licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE'      
+//     });
+    // var $isAnimatedSecond = $('.second .is-animated'),
+
+    // if( index == 1 && nextIndex == 2 ) {
+    //     $isAnimatedSecond.addClass('animated fadeInUpBig'); 
+    //     $isAnimatedSecond.eq(0).css('animation-delay', '.3s');
+    // }
+
+//})
+
+
