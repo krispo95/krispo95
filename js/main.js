d6bea95fde99043d1b411c4a9dc361f386d8bb92
Phlogistonny <pogosyan.chris@gmail.com>
2018-09-09 02:05:50 +0300

    new animation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-//  jQuery(document).ready(function ($) {
-//       $("#fullpage").fullpage({
-//         licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE'      
-//     });
-    // var $isAnimatedSecond = $('.second .is-animated'),
+jQuery(document).ready(function ($) {
+    var fullpage = $("#fullpage")
+    fullpage.fullpage({
+      licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+      onLeave: function (index, direction) {
+        //alert("Kristina is good!");
+        console.log(index);
+        var $isAnimatedLeft = $('#left');
+        var $isAnimatedCenter = $('#center');
+        var $isAnimatedRight = $('#right');
+        var $isAnimatedBottom = $('#bottom');
 
-    // if( index == 1 && nextIndex == 2 ) {
-    //     $isAnimatedSecond.addClass('animated fadeInUpBig'); 
-    //     $isAnimatedSecond.eq(0).css('animation-delay', '.3s');
-    // }
-
-//})
-
+        if (index.index == 0) {
+          $isAnimatedLeft.addClass('animated fadeInLeftBig');
+          $isAnimatedCenter.addClass('animated fadeInUpBig');
+          $isAnimatedRight.addClass('animated fadeInRightBig');
+          $isAnimatedBottom.addClass('animated zoomIn');
+          $isAnimatedBottom.eq(0).css('animation-delay', '.30s');
+        }
+      }
+    });
+  })
 
