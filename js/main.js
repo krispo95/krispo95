eafa7c2978b0a260d2753e20ec06fc6982a21c31
Phlogistonny <pogosyan.chris@gmail.com>
2018-09-13 00:09:11 +0300

    new animation

--- js/main.js
+++ js/main.js
@@ -5,20 +5,31 @@ jQuery(document).ready(function ($) {
       anchors:['firstPage', 'secondPage', 'thirdPage'],
       navigation:true,
       
-      onLeave: function (index, direction) {
+      onLeave: function (index, nextIndex, direction) {
         var $isAnimatedLeft = $('#left');
         var $isAnimatedCenter = $('#center');
         var $isAnimatedRight = $('#right');
         var $isAnimatedBottom = $('#bottom');
-        if (index.index == 0) {
+        if (index.index == 0 && direction == "down") {
           $isAnimatedLeft.addClass('animated fadeInLeftBig').css('animation-delay', '0.3s');
           $isAnimatedCenter.addClass('animated fadeInUpBig');
           $isAnimatedRight.addClass('animated fadeInRightBig').css('animation-delay', '0.6s');
           $isAnimatedBottom.addClass('animated zoomIn').css('animation-delay', '1.2s');
         }
-        if (index.index == 1) {
-          $('#example').addClass('animated zoomIn').css('animation-delay', '0.3s');
+        else if (index.index==2 && direction=="up"){
+          $isAnimatedLeft.removeClass('fadeInLeftBig fadeInLeft');
+          $isAnimatedLeft.addClass('fadeInLeft').css('animation-delay', '0.4s');
+          $isAnimatedCenter.removeClass('fadeInUpBig fadeInDown');
+          $isAnimatedCenter.addClass(' fadeInDown').css('animation-delay', '0.4s');
+          $isAnimatedRight.removeClass('fadeInRightBig fadeInRight ');
+          $isAnimatedRight.addClass('fadeInRight').css('animation-delay', '0.4s');
+          $isAnimatedBottom.removeClass('zoomIn fadeInDown');
+          $isAnimatedBottom.addClass('fadeInDown').css('animation-delay', '0.4s');
         }
+        // else if ((index.index == 2 && ) && nextIndex.index == 2) {
+        //   // $('#example').addClass('animated zoomIn').css('animation-delay', '0.4s');
+
+        // }
       }
     });
   })
